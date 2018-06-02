1467a4a4631db1d03312c37e52938b35efd18e71
quanrobi <luciarobi29@gmail.com>
2018-06-02 10:39:12 +0300

    control structure

---
+++ zari.py
@@ -0,0 +1,28 @@
+x = 6
+  
+  if x < 5
+
+        print("x is large")
+
+
+if x < 5 :
+  
+       print("x is small") 
+
+       else:
+
+       print("x is large")      
+
+
+
+  if x = = 0
+       print("x is zero") 
+
+     elif x > 0
+
+       print("x is positive") 
+
+       else
+
+       print("negative") 
+           
